021e415ee86f511950ce2dec76cdbac404836b56
william-cass-wright <william.cass.wright@gmail.com>
2022-07-21 22:11:32 -0700

    inital commit

---
+++ sys_config/__init__.py
@@ -0,0 +1,9 @@
+# type: ignore[attr-defined]
+"""
+Top-level package for sys-config.
+Awesome `sys-config` is a Python cli/package created with https://github.com/william-cass-wright/cookiecutter-pypackage-slim
+"""
+
+__author__ = """Will Wright"""
+__email__ = 'hello@sys-config.com'
+__version__ = '0.1.0'
