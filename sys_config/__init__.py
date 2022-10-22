788d127568f217800da24e3672a854bfa5a3f1e8
william-cass-wright <william.cass.wright@gmail.com>
2022-10-21 23:44:01 -0700

    added ConfigHandler to __init__.py

--- sys_config/__init__.py
+++ sys_config/__init__.py
@@ -7,3 +7,5 @@ Awesome `sys-config` is a Python cli/package created with https://github.com/wil
 __author__ = """Will Wright"""
 __email__ = "hello@sys-config.com"
 __version__ = "0.1.2"
+
+from sys_config import ConfigHandler
